ac7f54b2698faa64894e0fb5e9be2c1ce54defa7
데스크탑 <z_gahu@naver.com>
2025-02-07 20:44:49 +0900

    프라미스의 문제점 및 대안 예시

--- chapter5/callback-promise-async-await/async-await.js
+++ chapter5/callback-promise-async-await/async-await.js
@@ -3,4 +3,11 @@ async function myName() {
 }
 
 // async 를 사용하면 Promise로 반환값을 감싸서 넘겨준다.
-console.log(myName());
+// console.log(myName());
+
+async function showName() {
+  const name = await myName();
+  console.log(name);
+}
+
+console.log(showName());
