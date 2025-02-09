27b55db798910aecf45f963ee0ead32baa6a93ad
데스크탑 <z_gahu@naver.com>
2025-02-09 17:18:41 +0900

    답변추가

--- chapter5/callback-promise-async-await/async-await-exe.js
+++ chapter5/callback-promise-async-await/async-await-exe.js
@@ -2,11 +2,12 @@ async function a() {
   return "OK";
 }
 
-function b() {
-  const result = a();
+async function b() {
+  const result = await a();
   console.log(result);
 }
 
 b();
 
 // 문제: Promise {'OK'}  대신 'OK'가 출력되도록 수정
+// -> await 구문의 오른쪽에는 항상 Promise객체가 위치해야 한다.
