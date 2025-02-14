54e2b22f07e1154bd05f3d4fb4d4ffe3d5e12a65
데스크탑 <z_gahu@naver.com>
2025-02-15 00:51:44 +0900

    몽고디비연결테스트+env

---
+++ chapter6/try-mongo/test-connection.js
@@ -0,0 +1,32 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const dotenv = require("dotenv");
+dotenv.config();
+const uri = process.env.MONGO_URL;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //     "Pinged your deployment. You successfully connected to MongoDB!"
+    //   );
+    const adminDB = await client.db("test").admin();
+    const listDatabases = await adminDB.listDatabases();
+    console.log(listDatabases);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
