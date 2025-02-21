2645444e185453b42c16a67d70ffe327502c3965
데스크탑 <z_gahu@naver.com>
2025-02-21 22:32:52 +0900

    6.5.2 몽구스로 스키마 만들기

---
+++ chapter6/test-mongoose/person-model.js
@@ -0,0 +1,12 @@
+// 6.5.2 몽구스로 스키마 만들기
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+// 스키마 객체 생성
+const personSchema = new Schema({
+  name: String,
+  age: Number,
+  email: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Person", personSchema); // 모델 객체 생성
