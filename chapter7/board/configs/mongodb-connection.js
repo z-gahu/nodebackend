e845b29b583f3bf4e71f3d8b3e6181f20636cbc3
khjang <z_gahu@naver.com>
2025-02-23 00:12:32 +0900

    몽고디비 커넥션 추가가

---
+++ chapter7/board/configs/mongodb-connection.js
@@ -0,0 +1,8 @@
+const { MongoClient } = require("mongodb");
+// 몽고 디비 연결 주소
+const uri = process.env.MONGO_URL;
+
+module.exports = function (callback) {
+  // 몽고디비 커넥션 연결 함수 반환
+  return MongoClient.connect(uri, callback);
+};
