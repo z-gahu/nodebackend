b4d46061e004c911094694dfd66d49a8f8e3e850
데스크탑 <z_gahu@naver.com>
2025-02-24 21:41:57 +0900

    게시글 작성 같은 로직은 서비스 디렉토리 안에 위치하도록 생성

---
+++ chapter7/board/services/post-service.js
@@ -0,0 +1,12 @@
+//글쓰기
+async function writePost(collection, post) {
+  // 생성일시와 조회수를 넣기
+  post.hits = 0;
+  post.createdDt = new Date().toISOString(); //날짜는 ISO포멧으로저장
+  return await collection.insertOne(post); // 몽고디비에 post저장 후 결과 반환
+}
+
+module.export = {
+  // require()로 파일을 임포트시 외부로 노출하는 객체
+  wirtePost,
+};
