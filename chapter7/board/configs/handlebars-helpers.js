3d69acbd131705ce62ec5b1efc9b637e8aa4a81c
데스크탑 <z_gahu@naver.com>
2025-02-26 22:29:15 +0900

    템플릿 수정2: 리스트 및 페이지네이션 추가

--- chapter7/board/configs/handlebars-helpers.js
+++ chapter7/board/configs/handlebars-helpers.js
@@ -4,7 +4,7 @@ module.exports = {
   lengthOfList: (list = []) => list.length,
 
   // 두값을 비교해 같은지 여부를 반환
-  eq: (val, val2) => val1 === val2,
+  eq: (val1, val2) => val1 === val2,
 
   // ISO 날짜 문자열에서 날짜만 반환
   dateString: (isoString) => new Date(isoString).toLocaleDateString(),
